50421f2c5259125f8eb9453da544768f5dc4072d
Ltrone <1576763812@qq.com>
2025-12-19 17:44:47 +0800

    Add files via upload

---
+++ sydata.js
@@ -0,0 +1,10 @@
+var sydata = {
+	'code': 200,
+	'name': 'sy',
+	'rows': [{
+		'id': 1,
+		'img': '/lldq2.jpg',
+		'h5': '2023',
+		'p': '流浪地球2',
+	}]
+}
---
+++ ziydata.js
@@ -0,0 +1,8 @@
+var ziydata = {
+	'code': 200,
+	'name': 'ziy',
+	'rows': [{
+		'id': 1,
+		'p': '流浪地球2'
+	}]
+}
