4a93797f91a66de58770c4132e9fd325860132e4
Ltrone <1576763812@qq.com>
2025-12-30 20:20:53 +0800

    Update sydata.js

--- sydata.js
+++ sydata.js
@@ -3,7 +3,7 @@ var sydata = {
 	'name': 'sy',
 	'rows': [{
 		'id': 0,
-		'img': '/lldq2.jpg',
+		'img': 'https://cdn.sputniknews.cn/img/07e7/01/1b/1047408966_0:0:2048:2867_1920x0_80_0_0_0283a927c86527d33ad95832efda032a.jpg',
 		'h5': '2023',
 		'p': '流浪地球2',
 	}]
