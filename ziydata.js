4b5335d08cc8aaa3a8850051c8252411f6c6750e
Ltrone <1576763812@qq.com>
2025-12-31 20:10:03 +0800

    Add files via upload

--- ziydata.js
+++ ziydata.js
@@ -4,10 +4,10 @@ var ziydata = {
 	'rows': [{
 		'id': 0,
 		'p': '流浪地球2',
-		'video':'https://vip.dytt-live.com/20250505/5821_926c11cc/index.m3u8'
+		'video':'https://yzzy1.play-cdn16.com/20230414/7_d0737c10/index.m3u8'
 	},{
 		'id': 1,
 		'p': '画江湖之天罡',
-		'video':'https://vv.jisuzyv.com/play/zbqWOE7e/index.m3u8'
+		'video':'https://svip.high20-playback.com/20231229/10356_506b9c7c/index.m3u8'
 	}]
 }
