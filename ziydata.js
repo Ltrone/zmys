64c25113aeff9944e30cc46e1c4416fc14fbce16
Ltrone <1576763812@qq.com>
2025-12-31 20:45:22 +0800

    Add files via upload

--- ziydata.js
+++ ziydata.js
@@ -8,6 +8,6 @@ var ziydata = {
 	},{
 		'id': 1,
 		'p': '画江湖之天罡',
-		'video':'https://cdn16.yzzy-tv-cdn.com/20230414/7_d0737c10/index.m3u8'
+		'video':'https://cdn20.vip-vip-yzzy.com/20231229/10356_506b9c7c/index.m3u8'
 	}]
 }
