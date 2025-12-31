d22526b61fd43631350e36ab120262e3c041e26e
Ltrone <1576763812@qq.com>
2025-12-31 16:06:49 +0800

    Add files via upload

--- sydata.js
+++ sydata.js
@@ -6,5 +6,10 @@ var sydata = {
 		'img': 'https://cdn.sputniknews.cn/img/07e7/01/1b/1047408966_0:0:2048:2867_1920x0_80_0_0_0283a927c86527d33ad95832efda032a.jpg',
 		'h5': '2023',
 		'p': '流浪地球2',
+	},{
+		'id': 1,
+		'img': 'http://img.cjyun.org/a/10008/202208/8f8e5ecad80ae1aed00813c9d56d96da.jpeg',
+		'h5': '2023',
+		'p': '画江湖之天罡',
 	}]
 }
--- ziydata.js
+++ ziydata.js
@@ -5,5 +5,9 @@ var ziydata = {
 		'id': 0,
 		'p': '流浪地球2',
 		'video':'https://vip.dytt-live.com/20250505/5821_926c11cc/index.m3u8'
+	},{
+		'id': 1,
+		'p': '画江湖之天罡',
+		'video':'https://vip.dytt-hot.com/20250129/23197_a05b8b20/index.m3u8'
 	}]
 }
