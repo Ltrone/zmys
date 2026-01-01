1ba1c7803b0b6840a11baccca43e547b91c73aa2
Ltrone <1576763812@qq.com>
2026-01-01 15:39:55 +0800

    Add files via upload

--- ziydata.js
+++ ziydata.js
@@ -4,10 +4,10 @@ var ziydata = {
 	'rows': [{
 		'id': 0,
 		'p': '流浪地球2',
-		'video':'https://cdn16.yzzy-tv-cdn.com/20230414/7_d0737c10/index.m3u8'
+		'video':'https://vv.jisuzyv.com/play/neg6lR6d/index.m3u8'
 	},{
 		'id': 1,
 		'p': '画江湖之天罡',
-		'video':'https://cdn20.vip-vip-yzzy.com/20231229/10356_506b9c7c/index.m3u8'
+		'video':'https://vv.jisuzyv.com/play/zbqWOE7e/index.m3u8'
 	}]
 }
