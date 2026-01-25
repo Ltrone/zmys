3ef648d80956d8748faa31037d3e9fec176da7ac
Ltrone <1576763812@qq.com>
2026-01-25 14:11:40 +0800

    Add files via upload

--- ziydata.js
+++ ziydata.js
@@ -4,10 +4,10 @@ var ziydata = {
 	'rows': [{
 		'id': 0,
 		'p': '流浪地球2',
-		'video':'https://vip.dytt-live.com/20250505/5821_926c11cc/index.m3u8'
+		'video':'https://160.202.237.139:8899/m3u8.php?vkey=UFc4R1Y4WGRxamltN09obVVPRGJDcEFjQStKRDBmSkczV0Y0VmF6SEFQR3JhU0UvUmNTbysvaUlRUG4vZk1zTXpleGNQL2hSREVNPQ==.m3u8'
 	},{
 		'id': 1,
 		'p': '画江湖之天罡',
-		'video':'https://vip.dytt-hot.com/20250129/23197_a05b8b20/index.m3u8'
+		'video':'https://160.202.237.139:8899/m3u8.php?vkey=S3dwUGtMTDBBYytDbnBPSzRvN3dwY1ZmL1FwR2x4b29DZlVrOGx5NEtJQXBqdEQ1bDJvaWJQaUlRUG4vZk1zTWltdlJMZ0h2K2pRPQ==.m3u8'
 	}]
 }
